89ba65759db0d868ed51f4861c70e50a78e898aa
Iovannysm <oenjad@gmail.com>
2022-12-27 19:24:43 -0800

    Implemented the random.org api successfully.

---
+++ index.js
@@ -0,0 +1,34 @@
+const api_url = "https://api.random.org/json-rpc/2/invoke";
+async function randomNumber() {
+    const response = await fetch(api_url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+         body: JSON.stringify({
+            "jsonrpc": "2.0",
+            "method": "generateIntegers",
+            "params": {
+                "apiKey": "ba38db69-5c29-4029-85a4-4741eb902135",
+                "n": 4,
+                "min": 0,
+                "max": 7,
+                "replacement": true
+            },
+            "id": 42
+        })
+    });
+    const data = await response.json();
+    let randomNum = data.result.random.data;
+    
+    console.log(randomNum[0]);
+    console.log(randomNum[1]);
+    console.log(randomNum[2]);
+    console.log(randomNum[3]);
+}
+
+randomNumber();
+
+
+
+   
