9bd57d50b87905cc31fc55516f630501b1bcdf63
Iovannysm <oenjad@gmail.com>
2023-01-02 21:11:12 -0800

    Finsih the game, it is functional but it still needs error handeling

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+
+//This require below allows the game to be played in the terminal
 let prompt = require('prompt-sync')();
 // This is the API function to call the 4 random numbers
 // This variable api_url is the link to the Random.org api
 const api_url = "https://api.random.org/json-rpc/2/invoke";
 // This async function call of the api with the parameters needed to generate the 4 require numbers
-function randomNumber() {
+function randomNumber(){
+    // This fetch function is use to get the API call going.
     fetch(api_url , {
         method: 'POST',
         headers: {
@@ -24,71 +27,134 @@ function randomNumber() {
     }).then(response =>{
         return response.json();
     }).then(json => {
-        let data = json.result.random.data;
-        console.log(data);
-    
-    let correct = 0;
-    let tries = 0;
-    let rightNumberPlace = [];
+        // The nums variable access the random numbers from the API
+        let nums = json.result.random.data;
+        // console.log(nums);
+        // The tries variable tracks how many times you try to guess the numbers
+        let tries = 0;
+        // The chances variable tracks how many chances you have available before the games ends
+        let chances = 10;
+        // The rightDigit variable track the right numbers in the wrong location
+        let rightDigits = 0;
+        // The correct variable tracks the correct number on the correct location
+        let correct = 0;
+        // The guess array stores the players guesses
+        let guess = []
 
-    console.log("Welcome to the Mastermind Game!");
-    console.log("Rules are simple:"); 
-    console.log("Chose 4 numbers from 0 to 7.");
-    console.log("Try to guess the right number in the right order!");
-    console.log("You have 10 tries to figure this out.");
-    console.log("Now the game begins!");
+        // This are the rules given to the player
+        console.log("Welcome to the Mastermind Game!");
+        console.log("Rules are simple:"); 
+        console.log("Chose 4 numbers from 0 to 7.");
+        console.log("Try to guess the right number in the right order!");
+        console.log("You have 10 tries to figure this out.");
+        console.log("Numbers can be repeated");
+        console.log("Now the game begins!");
 
+        // This while loop runs until the correct variable is equal 4
+        while(correct < 4){
 
-    while(correct < 4){
-        let guess1 = prompt("First number ");
-        let guess2 = prompt("Second number ");
-        let guess3 = prompt("Third number ");
-        let guess4 = prompt("Fourth number ");
-        tries +=1;
+            // This are the prompt variables given to the player and stores the answer.
+            let guess1 = prompt("Choose the first number  ");
+            let guess2 = prompt("Choose the second number  ");
+            let guess3 = prompt("Choose the third number  ");
+            let guess4 = prompt("Choose the fourth number  ");
+            // Tires Increments by 1 each turn
+            tries += 1;
+            // Chances decreases by one each turn
+            chances -= 1;
 
-    //     // for(let i = 0; i < guess1; i++){
-    //     //     if(guess1[i] === response[i]){
-    //     //         correct += 1;
-    //     //         rightNumberPlace[i] = response[i];
-    //     //     }
-    //     // }
+            // This 4 if statements check to see if the player input is equal to the random numbers
+            // If the is not equal to the random number the player input is stored in the guess array
+            // The else statement check the opposite and  means that the player input is equal to the random number 
+            // it stores the number in the guess array and adds 1 to the correct variable
+            if(guess1 != nums[0]) {
+                guess.push(guess1);
+            }else {
+                guess.push(guess1);
+                correct += 1; 
+            };
 
-        if(guess1 == data[0]) {
-            correct += 1;
+            if(guess2 != nums[1]) {
+                guess.push(guess2);
+            }else {
+                guess.push(guess2);
+                correct += 1;
+            };
+
+            if(guess3 != nums[2]) {
+                guess.push(guess3);
+            }else {
+                guess.push(guess3);
+                correct += 1;
+            };
+
+            if(guess4 != nums[3]) {
+                guess.push(guess4);
+            }else {
+                guess.push(guess4);
+                correct += 1;
+            };
+
+            // This 4 if statements check the same thing but for each number in the guess array.
+            // This if statements check if the the player input is equal to the random number and adds 1 to the rightDigit variable
+            // Except it doesn't check if the guess array index is equal to the same index of nums array because the 4 previous if statements do that already.  
+            if(guess[0] == nums[1] || guess[0] == nums[2] || guess[0] == nums[3]){
+                rightDigits +=1;  
+            };
+            if(guess[1] == nums[0] || guess[1] == nums[2] || guess[1] == nums[3]){
+                rightDigits +=1;  
+            };
+            if(guess[2] == nums[0] || guess[2] == nums[1] || guess[2] == nums[3]){
+                rightDigits +=1; 
+            };
+            if(guess[3] == nums[0] || guess[3] == nums[1] || guess[3] == nums[2]){
+                rightDigits +=1;  
+            };
+               
+           
+
+            
+            // This if statement check to see if the correct variable is less than 4 
+            // and it gives logic to the player and informs the stage of the game played. 
+            if(correct < 4) {
+                if(rightDigits > 0){
+                    console.log('Player guesses ' + '" '+ guess + ' "'+ ' games responds ' + '" ' + rightDigits + ' correct numbers and ' + correct + ' correct locations " ');
+                    console.log('You have ' + chances + ' chances left!');
+                }else if(correct == 0){
+                    console.log('Player guesses ' + '" '+ guess + ' "'+ ' games responds ' + ' "all incorrect" ');
+                    console.log('You have ' + chances + ' chances left!');
+                }else if(correct == 1){
+                    console.log('Player guesses ' + '" '+ guess + ' "'+ ' games responds ' + '" ' + correct + ' correct number and ' + correct + ' correct location " ');
+                    console.log('You have ' + chances + ' chances left!');
+                }else if(correct > 1) {
+                    console.log('Player guesses ' + '" '+ guess + ' "'+ ' games responds ' + '" ' + correct + ' correct numbers and ' + correct + ' correct location " ');
+                    console.log('You have ' + chances + ' chances left!');
+                }
+                // This if statement check if the chances variable reaches 0 and terminates the game
+                if(chances == 0) {
+                    console.log("You ran out of chances, Better luck next time!");
+                    break
+                };
+
+                // This variable resets the variables and allows the while loop to continue and display the correct information
+                correct = 0;
+                rightDigits = 0;
+                guess = [];
+                continue
+            };    
+
+            // This if statement checks if the correct variable has reach 4 and ends the game with a win 
+            // message depending if the player won in 1 try or multiple tries.
+            if(correct == 4) {
+                if(tries == 1){
+                    console.log("You guess all the numbers!, It took you " + tries + " try.");
+                }else {
+                    console.log("You guess all the numbers!, It took you " + tries + " tries."); 
+                }
+            };
         };
-        if(guess2 == data[1]) {
-            correct += 1;
-        }
-        if(guess3 == data[2]) {
-            correct += 1;
-        }
-        if(guess4 == data[3]) {
-            correct += 1;
-        }
+    });    
+};
 
-        if (tries >= 3) {
-            console.log("You reach the maximum tries, Better luck next time");
-            break
-        }
-
-        if(correct < 4){
-            console.log("You guessed " + correct + " numbers correctly.");
-            correct = 0
-            continue
-        }else {
-            if(tries === 1 ){
-                console.log(" You did it!, you guess all the numbers correctly! It took you " + tries + " try");
-            }
-            else{
-                console.log(" You did it!, you guess all the numbers correctly! It took you " + tries + " tries");
-            }
-        } 
-    };
-
-
-    })
-
-    
-}
-
+// This is the call to the function that makes the game run when called in the terminal.
 randomNumber();
